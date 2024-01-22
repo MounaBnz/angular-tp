523a452181dea9f3054d0580640e454a947b205e
Mouna Ben Zaied <mouna.benzaied@insat.ucar.tn>
2024-01-22 13:27:07 +0100

    added some stuff

--- src/app/cvTech/cv.service.ts
+++ src/app/cvTech/cv.service.ts
@@ -11,6 +11,7 @@ export class CvService {
     new Personne (1,'Ben Zaied','Mouna', 23,'mouna.png',7777777,'Etudiante Mamrouja'),
     new Personne (2,'Ouhibi','Ons', 22,'ons.png',8888888,'Etudiante Mamrouja 2'),
     new Personne(3,'Makni','Mariem', 22,'mariem.png',6666666,'Etudiante Teeba'),
+    new Personne(4,'Kilani','Mariem', 22,'kila.png',444444444,'Etudiante Mamrouja 3'),
     new Personne(4,'Taghlet','Narjes', 22,'jess.jpg',5555555,'Etudiante Teeba 2')
 ];
 }
--- src/app/quizz-tine/quizz-tine.component.ts
+++ src/app/quizz-tine/quizz-tine.component.ts
@@ -21,8 +21,36 @@ export class QuizzTineComponent implements OnInit {
       options: ['Mediterranean Sea', 'Dead Sea', 'Red Sea', 'Black Sea'],
       correctAnswer: 'Mediterranean Sea'
     },
-    // Add more questions as needed
-  ];
+    {
+      question: 'What is the official language of Palestine?',
+      options: ['Arabic', 'Hebrew', 'English', 'French'],
+      correctAnswer: 'Arabic'
+    },
+    {
+      question: 'Which city is known for its historic market, or "souq," in Palestine?',
+      options: ['Nablus', 'Bethlehem', 'Jericho', 'Haifa'],
+      correctAnswer: 'Nablus'
+    },
+    {
+      question: 'In which year did the Nakba occur?',
+      options: ['1947', '1948', '1950', '1960'],
+      correctAnswer: '1948'
+    },
+    {
+      question: 'What is the traditional Palestinian dish made of minced meat and bulgur?',
+      options: ['Tabbouleh', 'Hummus', 'Kebbeh', 'Falafel'],
+      correctAnswer: 'Kebbeh'
+    },
+    {
+      question: 'Which Palestinian city is famous for its olive oil production?',
+      options: ['Hebron', 'Ramallah', 'Nablus', 'Jerusalem'],
+      correctAnswer: 'Nablus'
+    },
+    {
+      question: 'What is the significance of the Dome of the Rock in Jerusalem?',
+      options: ['Jewish Synagogue', 'Christian Church', 'Islamic Mosque', 'Buddhist Temple'],
+      correctAnswer: 'Islamic Mosque'
+    },  ];
 
   get currentQuestion(): any {
     return this.questions[this.currentQuestionIndex];
